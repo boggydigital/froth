2d24e23977efa64a80f144e4a7e201d85d51ab57
Bogdan Brinza <Bogdan.Brinza@gmail.com>
2021-03-06 17:34:25 -0800

    Adding initial implementation of froth on top of kvas

---
+++ stash.go
@@ -0,0 +1,82 @@
+package froth
+
+import (
+	"bytes"
+	"encoding/gob"
+	"github.com/boggydigital/kvas"
+)
+
+type Stash struct {
+	dst       string
+	asset     string
+	valueSet  *kvas.ValueSet
+	keyValues map[string]string
+}
+
+func NewStash(dst, asset string) (*Stash, error) {
+	kvStash, err := kvas.NewGobLocal(dst)
+	if err != nil {
+		return nil, err
+	}
+
+	stashRC, err := kvStash.Get(asset)
+	if err != nil {
+		return nil, err
+	}
+
+	var keyValues map[string]string
+
+	if stashRC != nil {
+		defer stashRC.Close()
+		if err := gob.NewDecoder(stashRC).Decode(&keyValues); err != nil {
+			return nil, err
+		}
+	}
+
+	if keyValues == nil {
+		keyValues = make(map[string]string, 0)
+	}
+
+	return &Stash{
+		dst:       dst,
+		asset:     asset,
+		valueSet:  kvStash,
+		keyValues: keyValues,
+	}, nil
+}
+
+func (stash *Stash) Set(key string, value string) error {
+
+	stash.keyValues[key] = value
+
+	return stash.write()
+}
+
+func (stash *Stash) write() error {
+	kvStash, err := kvas.NewGobLocal(stash.dst)
+	if err != nil {
+		return err
+	}
+
+	buf := new(bytes.Buffer)
+	if err := gob.NewEncoder(buf).Encode(stash.keyValues); err != nil {
+		return err
+	}
+
+	return kvStash.Set(stash.asset, buf)
+}
+
+func (stash *Stash) SetMany(keyValues map[string]string) error {
+	for k, v := range keyValues {
+		stash.keyValues[k] = v
+	}
+	return stash.write()
+}
+
+func (stash *Stash) Get(key string) (string, bool) {
+	if stash == nil || stash.keyValues == nil {
+		return "", false
+	}
+	val, ok := stash.keyValues[key]
+	return val, ok
+}
